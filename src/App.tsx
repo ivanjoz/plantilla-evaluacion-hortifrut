f618720ea315f9900cda8653d76835771b0642ae
ivanjoz <anguloivan3@gmail.com>
2022-11-22 09:42:14 -0500

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Evaluación Técnica - Hortifrut</h1>
+      <div className="card">
+        <p>
+          Edita el archivo <code>src/App.tsx</code> para comenzar
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
